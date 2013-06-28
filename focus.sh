d7d1b31b1c2679bc1cecd64cb04a0c6c6f2bb760
HolySparky <lalasjtu@gmail.com>
2013-06-28 17:44:20 +0800

    added judgement before modifying ipfw

--- focus.sh
+++ focus.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #To shut down dstport=80, so that you can focus!!!
+line=`sudo ipfw show | wc`
+rule_num=${line:0:8}
+echo $rule_num
 
+if [ $rule_num -eq 1 ]
+then
 #Block renren.com
-sudo ipfw add 1000 deny ip from any to 58.205.0.0/16
+    sudo ipfw add 1000 deny ip from any to 58.205.0.0/16
 #Block weibo.com
-sudo ipfw add 1000 deny ip from any to weibo.com
+    sudo ipfw add 1000 deny ip from any to weibo.com
 #Block Feedly.com
-sudo ipfw add 1000 deny ip from any to 65.19.0.0/16
+    sudo ipfw add 1000 deny ip from any to 65.19.0.0/16
 #Block SJTU_BBS
-sudo ipfw add 1000 deny ip from any to bbs.sjtu.edu.cn
+    sudo ipfw add 1000 deny ip from any to bbs.sjtu.edu.cn
+else
+    echo "Already focused, is there something else distracting you ?"
+fi
--- unfocus.sh
+++ unfocus.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #To shut down dstport=80, so that you can focus!!!
 
-sudo ipfw delete 1000
+lines=`sudo ipfw show | wc`
+rule_num=${lines:0:8}
+if [ $rule_num -gt 1 ]
+then
+    sudo ipfw delete 1000
+else
+    echo "You are not focusing right now, you still want that hum?"
+fi
